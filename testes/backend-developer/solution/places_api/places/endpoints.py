09da42a2e48f16512866b8ec47784dd16ba05738
Giovani Xavier Souza <giovani@2000info.com.br>
2019-12-18 11:37:43 -0300

    add endpoint to list registered places

--- testes/backend-developer/solution/places_api/places/endpoints.py
+++ testes/backend-developer/solution/places_api/places/endpoints.py
@@ -1,4 +1,23 @@
 from flask import Blueprint, jsonify, request, url_for, abort, make_response
 from flask_jwt import jwt_required
 
+from .models import Place
+
 app = Blueprint('places', __name__)
+
+
+@jwt_required()
+@app.route('/api/v1.0/places', methods=['GET'])
+def list_places():
+    try:
+
+        places = Place.query.filter().order_by('name')
+
+        return make_response(
+            jsonify(
+                places=[p.serialize for p in places]
+            ), 201
+        )
+
+    except Exception as ex:
+        abort(500)
